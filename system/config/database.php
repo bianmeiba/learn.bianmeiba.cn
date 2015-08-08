5d600dc04621c172ca7d58d3623091612b0de610
wangmingjob <wangmingjobo@gmail.com>
2015-08-09 05:34:43 +0800

    init

---
+++ system/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'bm_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'ming0032',
+  'dbname' => 'bianmeiba_learn',
+  'port' => '3306',
+);
+$config['slave'] = false;
+$config['port'] = '3306';
